a5ebec55a5bed7652c7db83982e11438b1b9d4a4
Paul Emms <pwemms@gmail.com>
2023-10-09 23:03:51 +0100

    Reformat comment

--- monads.R
+++ monads.R
@@ -1,3 +1,4 @@
 # examples of Monads in R
-# aka function factories - see https://www.brodrigues.co/blog/2022-04-11-monads/
+# aka function factories
+# see https://www.brodrigues.co/blog/2022-04-11-monads/
 # https://adv-r.hadley.nz/function-factories.html
